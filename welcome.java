07f7e0dd9805df8fbad1d6e45d3237bdbfbacea4
Manisha-Baheti <35602246+Manisha-Baheti@users.noreply.github.com>
2018-01-22 22:40:18 +0530

    Update welcome.java

--- welcome.java
+++ welcome.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class welcome extends Stage {
@@ -25,12 +27,26 @@ welcome(String user, String pass)
     grid.setVgap(10);
     grid.setPadding(new Insets(25, 25, 25, 25));
     
-    Label u1= new Label("Welcome");
+     Label u1= new Label("Welcome");
+    u1.setFont(new Font("Arial", 50));
+    u1.setTextFill(Color.FIREBRICK); 
     grid.add(u1, 0, 1);
-    Label u2= new Label(user);
-    grid.add(u2, 0, 3);
-    Label u3= new Label(pass);
-    grid.add(u3, 0, 5);
+    
+    Label u2= new Label("Username: ");
+    u2.setTextFill(Color.FIREBRICK); 
+    grid.add(u2, 0, 2);
+    
+    Label u3= new Label(user);
+    u3.setTextFill(Color.FIREBRICK); 
+    grid.add(u3, 1, 2);
+    
+    Label u4= new Label("Password: ");
+    u4.setTextFill(Color.FIREBRICK); 
+    grid.add(u4, 0, 3);
+    
+    Label u5= new Label(pass);
+    u5.setTextFill(Color.FIREBRICK); 
+    grid.add(u5, 1, 3);
 	
    
     this.setScene(new Scene(grid, 600, 600));
